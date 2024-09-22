63fe24acc3faa059d9b43f8b563e03bd7e942fb4
ZhiAn397 <tza0907@163.com>
2024-09-22 20:06:28 +0800

    frist

---
+++ src/main/java/com/tangzhian/newbookstores/NewBookstoresApplication.java
@@ -0,0 +1,13 @@
+package com.tangzhian.newbookstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBookstoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBookstoresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tangzhian/newbookstores/NewBookstoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangzhian.newbookstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBookstoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
